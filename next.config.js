d00453ef6982109514df4c48c7ba5ea13a9d2c56
texasroh <texasroh@gmail.com>
2023-08-10 22:42:06 +0900

    make env variable

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_PUBLIC_URL: process.env.PUBLIC_URL,
+    NEXT_PUBLIC_DATABASE_URL: process.env.DATABASE_URL,
+    NEXT_PUBLIC_SHADOW_DATABASE_URL: process.env.SHADOW_DATABASE_URL,
   },
 };
 
